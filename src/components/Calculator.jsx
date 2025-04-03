6f36b4e1ffccdcf5f2941f81a4dce90479644ea1
Madhav Gandhasiri <madhavgandhasiri@gmail.com>
2025-04-04 01:05:48 +0530

    Initial commit

---
+++ src/components/Calculator.jsx
@@ -0,0 +1,49 @@
+import React, {useState} from "react";
+import styles from "./Calculator.module.css";
+
+function Button({ text, setExpression, setResult, expression }) {
+  function handleClick(e){
+    if(text === 'C'){
+      setExpression("");
+      setResult("");
+    }else if(text === "="){
+      try{
+        setResult(eval(expression).toString());
+      }catch(error){
+        setResult("NaN")
+      }
+      
+    }else{
+      setExpression((prev)=> prev + text);
+    }
+    
+  }
+  return <button className={styles.singleBtn} onClick={handleClick}>{text}</button>;
+}
+
+function Calculator() {
+  const [expression, setExpression] = useState("");
+  const [result, setResult] = useState("");
+  function handleChange(e){
+    setExpression(e.target.value);
+  }
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.calculator}>
+        <h1>React Calculator</h1>
+        <input type="text" value={expression} onChange={handleChange}/>
+        {result.length > 0 ? (
+        <h2>{result}</h2>
+        ):(null)}
+        <div className={styles.btn}>
+          {['7','8','9','+','4','5','6','-','1','2','3','*','C','0','=','/'].map((char)=>(
+            <Button key={char} text={char} setExpression={setExpression} setResult={setResult} expression={expression} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Calculator;
